9b8f758f5b7bc3abb09dc009309c459bccea25c2
AbhishekWarad <abhishekwarad123@gmail.com>
2024-03-26 00:38:47 +0530

    add nested if-else.js

---
+++ JavaScript/nested if-else.js
@@ -0,0 +1,10 @@
+let marks = 83;
+
+if (marks >= 33) {
+    console.log("Pass");
+    if (marks >= 80){
+        console.log("Grade :A+");
+    }
+} else {
+    console.log("Better luck next time!");
+}

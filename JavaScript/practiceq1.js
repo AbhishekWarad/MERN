ac10174017c2e1b2482df4e0def4f3a3e5bb18cc
AbhishekWarad <abhishekwarad123@gmail.com>
2024-03-27 00:54:34 +0530

    add practiceq1.js

---
+++ JavaScript/practiceq1.js
@@ -0,0 +1,16 @@
+let a = "hiasd Warad";
+
+if ((a[0] === "a") && (a.length>3)) {
+    console.log("it's a good String");
+} else{
+    console.log("Not a Good String");
+} 
+
+
+let num = 12 ;
+
+if ((num%3 === 0) && ((num+1 == 15) || (num-1 == 11))) {
+    console.log("safe");
+} else {
+    console.log("unsafe");
+}

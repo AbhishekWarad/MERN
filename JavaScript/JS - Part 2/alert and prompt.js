88b876965e5a40c03f9c13b921f50efbb9182c7b
AbhishekWarad <abhishekwarad123@gmail.com>
2024-03-28 01:07:39 +0530

    add alert and prompt

---
+++ JavaScript/JS - Part 2/alert and prompt.js
@@ -0,0 +1,8 @@
+alert("This is an alert message : ")
+
+let firstname = prompt("Enter your first name :");
+let lastname = prompt("Enter your last name");
+
+let a = alert(`Your name : ${firstname} ${lastname}`);
+
+console.log(`${firstname} ${lastname}`);

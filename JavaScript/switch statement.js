4a06cc48ae8d2eb5ddcf4a9b9435b70f4201b885
AbhishekWarad <abhishekwarad123@gmail.com>
2024-03-27 23:58:22 +0530

    add switch statement.js

---
+++ JavaScript/switch statement.js
@@ -0,0 +1,16 @@
+let color = "red";
+
+switch (color) {
+    case "red":
+        console.log("STOP");
+        break;
+    case "yellow":
+        console.log("SLOW DOWN");
+        break;
+    case "green":
+        console.log("GO!");
+        break;
+    default:
+        console.log("You have entered wrong value.");
+        break;
+}

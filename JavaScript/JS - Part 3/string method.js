7675fb10e7c75f7f1229332e098cb159cb9f51ed
AbhishekWarad <abhishekwarad123@gmail.com>
2024-03-28 23:31:22 +0530

    add uppercase and lowercase

---
+++ JavaScript/JS - Part 3/string method.js
@@ -0,0 +1,8 @@
+let password =  "  abhishek warad adakljd    ";
+
+console.log(password.trim());
+
+
+// methods for converting strings to upeer and lower case  
+console.log(password.toUpperCase());
+console.log(password.toLowerCase());
--- JavaScript/JS - Part 3/string trim mrthod.js
+++
@@ -1,3 +0,0 @@
-let password =  "  abhishek warad adakljd    ";
-
-console.log(password.trim());

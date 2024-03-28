632e7d18aa3c3ded4bd4447270896c06cf443e01
AbhishekWarad <abhishekwarad123@gmail.com>
2024-03-28 23:56:39 +0530

    add slice method to string method.js

--- JavaScript/JS - Part 3/string method.js
+++ JavaScript/JS - Part 3/string method.js
@@ -6,3 +6,21 @@ console.log(password.trim());
 // methods for converting strings to upeer and lower case  
 console.log(password.toUpperCase());
 console.log(password.toLowerCase());
+
+
+// indexOf
+
+let str = "ILoveCoding";
+
+console.log(str.indexOf("Love"));
+console.log(str.indexOf("C"));
+console.log(str.indexOf("c"));
+
+
+// slice
+
+let str_slice = "ILoveCoding";
+
+console.log(str_slice.slice(1,5));
+console.log(str_slice.slice(1));
+console.log(str_slice.slice(-1,-5));

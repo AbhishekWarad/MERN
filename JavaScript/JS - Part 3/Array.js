551c811b368e5c1c18430e36bb9aa2cb768a3848
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-01 20:32:23 +0530

    array is mutable and indexing of Strings in an array

--- JavaScript/JS - Part 3/Array.js
+++ JavaScript/JS - Part 3/Array.js
@@ -10,5 +10,22 @@ console.log(marks);
 let names = ["Abhishek" , "ajhbdjahb" , "warad"];
 console.log(names);
 
-let info = ["Abhisehek Warad" , 2 , 5.6 , "male"];
+let info = ["Abhisehek Warad" , 2 , 5.6 , "male"];   //mixed array
 console.log(info);
+
+let newArr = [];
+
+
+// Indexing of srtings inside an array
+
+console.log(info[0][0]);
+console.log(info[0].length);
+
+// Array is mutable
+
+let fruits = ["Mango" , "Orange" , "Apple"];
+console.log(fruits);
+fruits[0] = "Bannana";
+
+console.log(fruits);
+

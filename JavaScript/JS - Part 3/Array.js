b0c54d5ebd3f15cf4b2c602487f3f7d8a37770d8
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-01 19:53:57 +0530

    add differnt methods of creating array

--- JavaScript/JS - Part 3/Array.js
+++ JavaScript/JS - Part 3/Array.js
@@ -1,3 +1,14 @@
 let students = ["aman" , "shradha" , "rajat"];
 
 console.log(students);
+
+// Differnt methods of creating an array
+
+let marks = [23,12,34,654,23];
+console.log(marks);
+
+let names = ["Abhishek" , "ajhbdjahb" , "warad"];
+console.log(names);
+
+let info = ["Abhisehek Warad" , 2 , 5.6 , "male"];
+console.log(info);

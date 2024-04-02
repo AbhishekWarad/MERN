3b804086e26f09af5f2feddf0f10eafd4352a880
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-02 22:56:56 +0530

    PQ on array

--- JavaScript/practiceQs/array.js
+++ JavaScript/practiceQs/array.js
@@ -4,3 +4,11 @@ months.shift();
 months.shift();
 
 months.unshift("june" , "July");
+
+
+let splice_month = ["january" , "july" , "march" , "august"];
+splice_month.splice(0,2,"July" , "june");
+
+
+let lang = ["c" , "c++" , "html" , "javascript" , "python", "java" , "c#", "sql"]
+console.log('lang.reverse().indexOf("javascript") : ' + lang.reverse().indexOf("javascript"));

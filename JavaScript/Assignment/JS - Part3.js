6557b3a13ebe562680f932e4955327e07a8be3dd
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-03 00:26:18 +0530

    part 3 assignment

---
+++ JavaScript/Assignment/JS - Part3.js
@@ -0,0 +1,39 @@
+// PQ 1
+
+let arr = [1,2,3,4,5,6,7,8];
+let n = 3;
+console.log(arr.slice(0 , n));
+
+
+// PQ 2
+
+console.log(arr.slice(-n));
+
+
+// PQ 3
+
+let string = "sd";
+
+if (string.length == 0) {
+    console.log("EMPTY STRING");
+} else {
+    console.log("NOT EMPTY STRING");
+}
+
+// PQ 4
+
+let s = "  Abhishek Warad  ";
+
+console.log("SPACE TRIM : "+s.trim());
+
+
+// PQ 5
+
+let array = [1,2,3,4,5];
+let num = 6;
+
+if(array.includes(num) == true){
+    console.log("EXISTS");
+} else {
+    console.log("NOT EXISTS");
+}

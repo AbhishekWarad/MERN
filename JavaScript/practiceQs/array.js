80df17ab1cc733c1e177983ff9d67204660e25da
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-02 23:55:54 +0530

    PQ tic-tac-toe

--- JavaScript/practiceQs/array.js
+++ JavaScript/practiceQs/array.js
@@ -12,3 +12,9 @@ splice_month.splice(0,2,"July" , "june");
 
 let lang = ["c" , "c++" , "html" , "javascript" , "python", "java" , "c#", "sql"]
 console.log('lang.reverse().indexOf("javascript") : ' + lang.reverse().indexOf("javascript"));
+
+
+
+// -------------------------- TIC-TAC-TOE --------------------------------
+
+const tic_tac_toe = [ ['X',null,'O'] , [null,'X',null] , ['O',null,'X'] ];

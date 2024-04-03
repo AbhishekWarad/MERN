c05e2583a2bd51505d3c5f09b51bfb48771dccd3
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-03 23:01:16 +0530

    while loop

--- JavaScript/JS - Part 4/forloop.js
+++ JavaScript/JS - Part 4/forloop.js
@@ -1,6 +1,15 @@
 // print multiples of n
- let n = prompt("enter n");
+// let n = prompt("enter n");
+// n = parseInt(n);
+// for (let index = 1; index <= 10; index++) {
+// console.log(n * index );  
+// };
 
- for (let index = 1; index <= 10; index++) {
-    console.log(n * index );  
- };
+// WHILE LOOP 
+
+let i = 1;
+
+while (i<=5) {
+    console.log(i);
+    i++;
+}

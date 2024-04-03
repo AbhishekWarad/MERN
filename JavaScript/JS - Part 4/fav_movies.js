89c9a6efc58bfb3317f80ffc7e327244bbc3125d
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-03 23:29:57 +0530

    update fav_movie code

--- JavaScript/JS - Part 4/fav_movies.js
+++ JavaScript/JS - Part 4/fav_movies.js
@@ -3,3 +3,9 @@ let guess = prompt("Enter your fav movie : ");
 while((guess != "Avatar") && (guess != "quit")){
     guess=prompt("Enter again : ")
 };
+
+if (guess == 'Avatar') {
+    console.log("You Guessed");
+} else {
+    console.log("you quit");
+}

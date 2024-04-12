9866c88d5808d2cdc6167ac72015addbdeca1798
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-13 00:03:08 +0530

    add part5.html, object literals.js and create object called post

---
+++ JavaScript/JS - Part 5/object literals.js
@@ -0,0 +1,9 @@
+// create object for a thread/twitter post 
+
+const post ={
+    usename: "@abhishek warad",
+    content: "This is my first post",
+    likes: 150,
+    reposts: 5,
+    tags : ["@apnacollege","@elonmusk"]
+}

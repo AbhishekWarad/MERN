4f545aeac06b80ef3e093d9b0804f79edee9b023
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-13 00:27:30 +0530

    get values property in object literal

--- JavaScript/JS - Part 5/object literals.js
+++ JavaScript/JS - Part 5/object literals.js
@@ -7,3 +7,27 @@ const post ={
     reposts: 5,
     tags : ["@apnacollege","@elonmusk"]
 }
+
+console.log(post["username"]);
+console.log(post.usename);
+
+const obj = {
+    1:"a",
+    2:'b',
+    null: "c",
+    undefined: "d",
+    true: "e",
+    ok:'f'
+};
+
+console.log(obj[1]);
+console.log(obj[2]);
+console.log(obj[null]);
+console.log(obj[undefined]);
+console.log(obj[true]);
+console.log(obj[ok]);
+console.log(obj["ok"]);
+
+// console.log(obj.1);
+console.log(obj.null);
+console.log(obj.undefined);

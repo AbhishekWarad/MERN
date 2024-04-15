2d9f8349bdf20c50057c6acd250f0addc9e3e4fa
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-16 00:21:00 +0530

    solve pq1 of js part6

---
+++ JavaScript/practiceQs/part6 functions.js
@@ -0,0 +1,8 @@
+// pq1 - create a function to print a poem
+
+function printPoem() {
+    console.log("Twinkle Twinkle, little star");
+    console.log("How I wonder,What you are");
+}
+
+printPoem();

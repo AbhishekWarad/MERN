e38767ef1db55076fd8d5df482dd9b5b198cf6eb
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-17 21:19:15 +0530

    solve qs 2 ,3 of js part 6 assignment

--- JavaScript/Assignment/JS - part6.js
+++ JavaScript/Assignment/JS - part6.js
@@ -9,4 +9,45 @@ function largerNumber(a,n){
         }
     }
 }
-largerNumber(a,3);
+// largerNumber(a,3);
+
+
+// qs 2 - Write a JS function to extract unique characters from a string.
+// EX - str = "abcddeeffggh"
+//      ans = "abcdefgh"
+
+function uniqueString(string) {
+    let result = "";
+    for(let i=0; i<string.length; i++){
+        let currChar = string[i];
+        if(result.indexOf(currChar) == -1){
+            result = result + currChar;
+        }
+    }
+    return result;
+}
+
+console.log(uniqueString("abhishek"));
+
+
+//qs 3 - Write a JS fnction that accepts a list of contry names as input and returns the longest country name as output.
+// EX - country = ["Australia","Germany","United States of America"]
+//      output = "United States of America"
+
+function longestString(arr) {
+    let largest = 0;
+    let len = 0;
+    for(let i=0 ; i < arr.length ; i++){
+        if(arr[i].length > len){
+            len = arr[i].length;
+            largest = i;
+            temp = 0;
+        }
+    }
+
+    console.log(arr[largest]);
+}
+
+
+let country=["Australia","Germany","United States of America"];
+console.log(longestString(country));

d88a4164718279a47feb003f922b5eba3ef1146e
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-17 20:01:51 +0530

    solve qs1 of js part 6 assignment

--- JavaScript/Assignment/JS - part6.js
+++ JavaScript/Assignment/JS - part6.js
@@ -1,7 +1,12 @@
-// qs1 
+// qs1 - Write a JavaScript funcction that returns unique characters from a string
 
-let arr = [1,2,3,4,5];
+let a = [8,9,10,1,2,3,4,5,6,7];
 
-function largerNumber(){
-    
+function largerNumber(a,n){
+    for(i=0 ; i<= a.length ; i++){
+        if (a[i] > n){
+            console.log(a[i]);
+        }
+    }
 }
+largerNumber(a,3);

4402a6c2614c5db80c9c5c80c8d9314a5ec579e2
AbhishekWarad <abhishekwarad123@gmail.com>
2024-04-19 01:28:40 +0530

    solve pq2 of js part 7

--- JavaScript/practiceQs/JS part7.js
+++ JavaScript/practiceQs/JS part7.js
@@ -3,3 +3,19 @@
 const squareN = (n) => (n*n);
 
 console.log(squareN(2));
+
+
+// pq2 -
+
+function print() {
+    let id = setInterval(() => {
+        console.log("Hello world");
+    } ,2000);
+    setTimeout(() => {
+        clearInterval(id);
+        console.log("clear interval")
+    },10000);
+};
+
+print();
+

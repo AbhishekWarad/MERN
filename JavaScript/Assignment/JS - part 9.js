3ae039c3f26dff2a235e810edc936bd7e349cde8
AbhishekWarad <abhishekwarad123@gmail.com>
2024-05-08 02:04:43 +0530

    solved assignment question 2

--- JavaScript/Assignment/JS - part 9.js
+++ JavaScript/Assignment/JS - part 9.js
@@ -1,8 +1,20 @@
 // QS-1
 
+let input = document.createElement("input");
 let button = document.createElement("button");
 let body = document.querySelector("body");
 
-button.innerText = "Click me";
 
+button.innerText = "Click me"
+
+body.append(input);
 body.append(button);
+
+//QS-2
+
+input.setAttribute("placeholder","Username");
+button.setAttribute("id" , "btn");
+
+
+//QS-3
+

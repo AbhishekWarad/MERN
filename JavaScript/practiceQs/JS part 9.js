e1cf37c2268d3429a06dd6edfe6d5a861ae538d4
AbhishekWarad <abhishekwarad123@gmail.com>
2024-05-08 00:34:21 +0530

    solve pq 2

--- JavaScript/practiceQs/JS part 9.js
+++ JavaScript/practiceQs/JS part 9.js
@@ -3,3 +3,8 @@ para1.innerText = "Hey I'm red";
 document.querySelector('body').appendChild(para1);
 para1.style.color = "red";
 
+let heading3 = document.createElement('h3');
+heading3.innerText = "I'm a blue h3!"
+heading3.style.color = "blue";
+document.querySelector('body').insertAdjacentElement("afterbegin",heading3);
+

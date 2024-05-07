828611599475f7098f74c144a8565cb72fd8acf9
AbhishekWarad <abhishekwarad123@gmail.com>
2024-05-08 00:48:25 +0530

    solved pq3

--- JavaScript/practiceQs/JS part 9.js
+++ JavaScript/practiceQs/JS part 9.js
@@ -8,3 +8,14 @@ heading3.innerText = "I'm a blue h3!"
 heading3.style.color = "blue";
 document.querySelector('body').insertAdjacentElement("afterbegin",heading3);
 
+let div = document.createElement('div');
+div.style.border = "2px solid black";
+document.querySelector('body').appendChild(div);
+let divh1 = document.createElement('h1');
+divh1.innerText = "I'm in a div";
+
+document.querySelector('div').appendChild(divh1);
+
+let divp = document.createElement('p');
+divp.innerText = "ME TOO!";
+document.querySelector('div').appendChild(divp);

75a32509d4e4fe0f5923f599b671a1710cfe0e7c
AbhishekWarad <abhishekwarad123@gmail.com>
2024-05-08 20:01:26 +0530

    addEventListerer

--- JavaScript/JS - Part 10/app.js
+++ JavaScript/JS - Part 10/app.js
@@ -4,17 +4,34 @@
 // }
 
 
+// let btns = document.querySelectorAll('button');
+
+// for(i of btns){
+//     i.onclick = sayHello;
+//     i.onmouseenter = printBye;
+// }
+
+// function sayHello() {
+//     alert("hello!");
+// }
+
+
+// function printBye(){
+//     console.log("your mouse enterd on button!")
+// }
+
+
 let btns = document.querySelectorAll('button');
 
-for(i of btns){
-    i.onclick = sayHello;
-    i.onmouseenter = printBye;
+for(btn of btns){
+    btn = addEventListener("click" , sayHello);
+    btn = addEventListener("click" , printBye);
+
 }
 
 function sayHello() {
     alert("hello!");
 }
-
 
 function printBye(){
     console.log("your mouse enterd on button!")

d96ba4f9d7d9be70728a3731638c05bef7b66349
AbhishekWarad <abhishekwarad123@gmail.com>
2024-05-08 19:34:41 +0530

    onclick and onmouseenter events

---
+++ JavaScript/JS - Part 10/app.js
@@ -0,0 +1,21 @@
+// let btn = document.querySelector('button');
+// btn.onclick = function (){
+//     alert("button has been pressed!");
+// }
+
+
+let btns = document.querySelectorAll('button');
+
+for(i of btns){
+    i.onclick = sayHello;
+    i.onmouseenter = printBye;
+}
+
+function sayHello() {
+    alert("hello!");
+}
+
+
+function printBye(){
+    console.log("your mouse enterd on button!")
+}

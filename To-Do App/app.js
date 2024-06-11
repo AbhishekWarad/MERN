44e8df33e99643b0e7b94d784f3467c4c75b6346
AbhishekWarad <abhishekwarad123@gmail.com>
2024-06-11 12:23:50 +0530

    learn how to add

--- To-Do App/app.js
+++ To-Do App/app.js
@@ -1,8 +1,12 @@
 let input = document.querySelector("input");
 let btn = document.querySelector("button");
-let li = document.querySelectorAll("li");
+let ul = document.querySelector("ul");
 
 btn.addEventListener("click", function(){
-    
+    let item = document.createElement("li");
+    console.log(input.value);
+    item.innerText = input.value;
+    ul.appendChild(item);
+    input.value = "";
 })
 

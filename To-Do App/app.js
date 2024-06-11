3801875e3199e665a4f1e66eccfd40423266ee65
AbhishekWarad <abhishekwarad123@gmail.com>
2024-06-11 13:25:17 +0530

    deleting buttons

--- To-Do App/app.js
+++ To-Do App/app.js
@@ -4,9 +4,24 @@ let ul = document.querySelector("ul");
 
 btn.addEventListener("click", function(){
     let item = document.createElement("li");
-    console.log(input.value);
     item.innerText = input.value;
+
+    let delbtn = document.createElement("button");
+    delbtn.innerText = "delete";
+    delbtn.classList.add("delete");
+    
+    item.appendChild(delbtn);
     ul.appendChild(item);
     input.value = "";
 })
 
+let delbtns = document.querySelectorAll(".delete");
+
+for(del of delbtns){
+    del.addEventListener("click" , function(){
+        console.log("element delted");
+        let par = this.parentElement;
+        console.log(par);
+        par.remove();
+    })
+}
